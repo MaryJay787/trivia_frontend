80b47235b6e63f4e67ac99736e9a283f1da4719e
MaryJay787 <josephmaryam.mj@gmail.com>
2019-12-27 18:57:42 -0500

    header added to landing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header, Container } from 'semantic-ui-react'
+import { Header, Container, Card } from 'semantic-ui-react'
 import './App.css';
 
 class App extends React.Component {
@@ -9,9 +9,14 @@ class App extends React.Component {
         <Header as='h1'> Trivia Games </Header>
         <Container>
           Game Board Goes Here
+          <Card.Group>
+            <Card fluid color='red' header='Option 1' />
+            <Card fluid color='orange' header='Option 2' />
+            <Card fluid color='yellow' header='Option 3' />
+          </Card.Group>
         </Container>
       </div>
-    );
+    )
   }
 }
 
