b96a30bf2fafad9dc7ab384182e2a2ad941519f1
MaryJay787 <josephmaryam.mj@gmail.com>
2020-01-05 21:54:25 -0500

    altering game aboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import { Header, Container, Card } from 'semantic-ui-react'
+import { Header, Container, Card, Button } from 'semantic-ui-react'
 import { getCategories } from './fetches/backend.js'
 import { connect } from 'react-redux';
 import './App.css';
@@ -14,12 +14,42 @@ class App extends React.Component {
     return(
       <div>
         <Header as='h1' textAlign='center' color='green'> Trivia Games </Header>
-        <Container>
+        <Container textAlign='center'>
           Game Board Goes Here
           <Card.Group>
-            <Card fluid color='red' header='Option 1' />
-            <Card fluid color='orange' header='Option 2' />
-            <Card fluid color='yellow' header='Option 3' />
+            
+            <Card color='red' >
+              <Card.Content header='Ouestion 1' />
+                <Card.Content description='sdkjfdjklf jdfsj jdfksldh f jshlfkdjf jsldfkjsdh lsjdhjdfhls sdhkj ' />
+                <Card.Content extra>
+                <Button size='mini'>Click Here</Button>
+              </Card.Content>   
+            </Card>
+
+            <Card color='orange' >
+              <Card.Content header='Ouestion 2' />
+                  <Card.Content description='sdkjfdjklf jdfsj jdfksldh f jshlfkdjf jsldfkjsdh lsjdhjdfhls sdhkj ' />
+                  <Card.Content extra>
+                  <Button size='mini'>Click Here</Button>
+              </Card.Content>   
+            </Card>
+
+            <Card color='yellow' >
+              <Card.Content header='Ouestion 3' />
+                  <Card.Content description='sdkjfdjklf jdfsj jdfksldh f jshlfkdjf jsldfkjsdh lsjdhjdfhls sdhkj ' />
+                  <Card.Content extra>
+                  <Button size='mini'>Click Here</Button>
+              </Card.Content>   
+            </Card>
+
+            <Card color='blue' >
+              <Card.Content header='Ouestion 4' />
+                  <Card.Content description='sdkjfdjklf jdfsj jdfksldh f jshlfkdjf jsldfkjsdh lsjdhjdfhls sdhkj ' />
+                  <Card.Content extra>
+                  <Button size='mini'>Click Here</Button>
+              </Card.Content>   
+            </Card>
+
           </Card.Group>
         </Container>
       </div>
